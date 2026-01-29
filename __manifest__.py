4977e25c51c76802d3ae18685472e5f0815985bf
Nakari18 <nakaostos@gmail.com>
2026-01-29 00:10:21 -0400

    estate

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'author': 'Marian Bracho',
+    'website': 'https://www.tu-sitio-web.com',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
